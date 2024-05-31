473a1acf54ef1a463d53de8bc4c12eb338e63eb3
Mizaan-hub <mizaanafandi@gmail.com>
2024-05-31 22:36:03 +0530

    updated userInfo componenets

--- src/components/list/userInfo/userInfo.jsx
+++ src/components/list/userInfo/userInfo.jsx
@@ -2,7 +2,17 @@ import './userInfo.scss'
 
 function UserInfo (){
     return(
-        <div className='UserInfo'>UserInfo</div>
+        <div className='UserInfo'>
+            <div className="user">
+                <img src="/public/avatar.png" alt="" />
+                <h2>Username</h2>
+            </div>
+            <div className="icons">
+                <img src="/public/more.png" alt="" />
+                <img src="/public/video.png" alt="" />
+                <img src="/public/edit.png" alt="" />
+            </div>
+        </div>
     )
 }
 export default UserInfo
