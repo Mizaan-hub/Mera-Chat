2d009a382cfd677df604eb9fa7b3991c1bbd7d5b
Mizaan-hub <mizaanafandi@gmail.com>
2024-05-31 23:22:55 +0530

    Updaed chatList component

--- src/components/list/chatList/chatList.jsx
+++ src/components/list/chatList/chatList.jsx
@@ -1,8 +1,20 @@
+import { useState } from 'react';
 import './chatList.scss'
 
 function ChatList (){
+
+    const[addMode, setAddMode] = useState(false);
+
     return(
-        <div className='ChatList'>ChatList</div>
+        <div className='ChatList'>
+            <div className="search">
+                <div className="searchBar">
+                    <img src="/public/search.png" alt="" />
+                    <input type="search"  placeholder='Search'/>
+                </div>
+                <img src={addMode? "/public/minus.png":"/public/plus.png"} alt="" className='add' onClick={ ()=> setAddMode(!addMode)}/>
+            </div>
+        </div>
     )
 }
 export default ChatList
