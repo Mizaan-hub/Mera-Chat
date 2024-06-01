4418f5a5f9a80645b69a06c0de2e0b2cd1645f2a
Mizaan-hub <mizaanafandi@gmail.com>
2024-06-02 03:28:00 +0530

    Working searchbar to add user

--- src/components/list/chatList/addUser/addUser.jsx
+++ src/components/list/chatList/addUser/addUser.jsx
@@ -1,20 +1,44 @@
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import {db} from '../../../../lib/firebase'
 import './addUser.scss'
+import { useState } from 'react';
 
 const AddUser = () => {
+
+    const[user, setUser] = useState(null)
+
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const username = formData.get("username");
+
+        try {
+            const userRef = collection(db,"users")
+            const q = query(userRef, where("username", "==", username));
+            const querySnapshot = await getDocs(q);
+
+            if(!querySnapshot.empty){
+                setUser(querySnapshot.docs[0].data())
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
     return(
         <div className='AddUser'>
-            <form action="">
+            <form onSubmit={handleSearch}>
                 <input type="search" name="username" placeholder="Username" />
                 <button>Search</button>
             </form>
-            <div className="user">
+            {user && <div className="user">
                 <div className="detail">
-                    <img src="/public/avatar.png" alt="" />
-                    <span>Username</span>
+                    <img src={user.avatar || "/public/avatar.png"} alt="" />
+                    <span>{user.username}</span>
                 </div>
                 <button>Add User</button>
                 
-            </div>
+            </div>}
         </div>
     )
 }
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -3,7 +3,7 @@ import './login.scss'
 import { toast } from 'react-toastify'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth"
 import { auth, db } from '../../lib/firebase'
-import { doc, setDoc } from 'firebase/firestore'
+import { collection, doc, getDocs, query, setDoc, where } from 'firebase/firestore'
 import upload from '../../lib/upload'
 
 const Login = () => {
@@ -49,7 +49,20 @@ const Login = () => {
         setLoading(true)
 
         const formData = new FormData(e.target)
-        const { username, email, password} = Object.fromEntries(formData)
+        const { username, email, password} = Object.fromEntries(formData);
+
+        // VALIDATE INPUTS
+    if (!username || !email || !password)
+        return toast.warn("Please enter inputs!");
+    if (!avatar.file) return toast.warn("Please upload an avatar!");
+
+      // VALIDATE UNIQUE USERNAME
+    const usersRef = collection(db, "users");
+    const q = query(usersRef, where("username", "==", username));
+    const querySnapshot = await getDocs(q);
+    if (!querySnapshot.empty) {
+        return toast.warn("Select another username");
+    }
 
         try {
             const userCredential = await createUserWithEmailAndPassword(auth,email,password);
