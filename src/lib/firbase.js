9063e0817be3a13bf2d636b79014d9c7f2b50443
Mizaan-hub <mizaanafandi@gmail.com>
2024-06-01 20:03:00 +0530

    Added firebase auth, db and storage

---
+++ src/lib/firbase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: "mera-chat-69.firebaseapp.com",
+    projectId: "mera-chat-69",
+    storageBucket: "mera-chat-69.appspot.com",
+    messagingSenderId: "115374613165",
+    appId: "1:115374613165:web:c450c0cf6ff346377a1c24"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth()
+export const db = getFirestore()
+export const storage = getStorage()
