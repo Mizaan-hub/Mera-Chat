bbed8a6ebe49c4153193e83d4545503c41de242f
Mizaan-hub <mizaanafandi@gmail.com>
2024-06-02 02:04:07 +0530

    Updated chat list with realtime data

--- src/components/list/chatList/chatList.jsx
+++ src/components/list/chatList/chatList.jsx
@@ -1,11 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './chatList.scss'
 import AddUser from './addUser/addUser';
+import { useUserStore } from '../../../lib/userStore';
+import { doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { db } from '../../../lib/firebase';
 
 
 function ChatList (){
 
     const[addMode, setAddMode] = useState(false);
+    const[chats, setChats] = useState([]);
+
+    const {currentUser} = useUserStore()
+
+    useEffect (() => {
+        const unSub = onSnapshot(
+            doc(db, "userChats", currentUser.id), 
+            async (userCredential) => {
+            const items = userCredential.data().chats;
+
+            const promises = items.map(async (item) => {
+                const userDocRef = doc(db, "users", item.receiver.id)
+                const userDocSnap = await getDoc(userDocRef);
+
+                const user =  userDocSnap.data()
+
+                return{
+                    ...item, user
+                };
+            });
+
+            const chatData = await Promise.all(promises)
+            setChats(chatData.sort((a,b) => b.updateAt - a.updateAt))
+            }
+        );
+
+        return () => {
+            unSub()
+        }
+    },[currentUser.id])
 
     return(
         <div className='ChatList'>
@@ -21,8 +54,17 @@ function ChatList (){
                     alt="" className='add' 
                     onClick={() => setAddMode(!addMode)}/>
             </div>
-
-            <div className="item">
+                
+                {chats.map(chat => (
+                    <div className="item" key={chat.chatId}>
+                    <img src="/public/avatar.png" alt="" />
+                    <div className="texts">
+                        <span>UserName</span>
+                        <p>{chat.lastMessage}</p>
+                    </div>
+                </div>
+                ))}
+            {/* <div className="item">
                 <img src="/public/avatar.png" alt="" />
                 <div className="texts">
                     <span>UserName</span>
@@ -56,14 +98,7 @@ function ChatList (){
                     <span>UserName</span>
                     <p>Hello World</p>
                 </div>
-            </div>
-            <div className="item">
-                <img src="/public/avatar.png" alt="" />
-                <div className="texts">
-                    <span>UserName</span>
-                    <p>Hello World</p>
-                </div>
-            </div>
+            </div> */}
 
         {addMode && <AddUser/>}
 
